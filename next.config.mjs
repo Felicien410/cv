e357a86c53c4e087f7aa232005b94284f17e5cbb
Félicien Catteau <97636902+Felicien410@users.noreply.github.com>
2025-05-09 13:05:19 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
-// next.config.mjs
-import './src/debug.js';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // Nécessaire pour GitHub Pages
+    basePath: process.env.NODE_ENV === 'production' ? '/cv-felicien' : '', // Remplacez 'cv-felicien' par le nom de votre dépôt GitHub
+    images: {
+      unoptimized: true, // Nécessaire pour l'export statique
+    },
+    trailingSlash: true, // Recommandé pour les sites statiques
+  };
+  
+  export default nextConfig;
