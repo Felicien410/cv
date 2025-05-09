720a2f1b7052d0eba4aed460620870a5da40c272
Félicien Catteau <97636902+Felicien410@users.noreply.github.com>
2025-05-09 14:58:28 +0200

    Updates

--- src/debug.js
+++
@@ -1,6 +0,0 @@
-// src/debug.js
-console.log('=== DEBUGGING TAILWIND ===');
-console.log('Node version:', process.version);
-console.log('=========================');
-
-// Ne pas essayer de résoudre les chemins pour le moment
